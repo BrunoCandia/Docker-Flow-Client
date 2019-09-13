6a9c1b414c37cc4aecde2056bbfd10df90ed2b2c
Bruno Candia <brunocandia@gmail.com>
2019-09-13 16:35:19 -0300

    Changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CHANGED!!!
+          Learn React CHANGED ON THE FEATURE BRANCH!!!
         </a>
       </header>
     </div>
